3b44feb010fcce682f690aa87bec93d45ceaeba8
lgm527 <laurellmccaffrey@gmail.com>
2020-04-09 16:57:29 -0400

    logo added and base html updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import tt2 from './assets/tt2.png';
 import './style/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={tt2} className="tt2-logo" alt="logo" />
       </header>
     </div>
   );
