792ce542700c1e5d0c497e412c94a14580b8924a
lgm527 <laurellmccaffrey@gmail.com>
2020-04-13 13:40:48 -0400

    more map updates

--- src/containers/TreeContainer.js
+++ src/containers/TreeContainer.js
@@ -92,8 +92,7 @@ normalizeString = (str) => {
               zoom={14}
               initialCenter={{lat: 40.703316, lng: -73.988145}}
               center={{lat: 40.703316, lng: -73.988145}}
-              style={{cursor: 'pointer', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}
-              yesIWantToUseGoogleMapApiInternals={true}
+              style={{cursor: 'pointer', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', height: '100%'}}
               >
               {theTrees}
             </Map> }
