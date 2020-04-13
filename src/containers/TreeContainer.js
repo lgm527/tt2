1509c219e7917401de6e9593c21f2c0c462922d4
lgm527 <laurellmccaffrey@gmail.com>
2020-04-13 15:10:15 -0400

    map

--- src/containers/TreeContainer.js
+++ src/containers/TreeContainer.js
@@ -67,6 +67,7 @@ normalizeString = (str) => {
 
   render() {
     const { normalizeString, handleClick, backToMap } = this;
+    const wburg = {lat: 40.714700, lng: -73.956120}
 
     const theTrees = this.state.trees.map((tree) => {
       return <Marker
@@ -90,9 +91,9 @@ normalizeString = (str) => {
           : <Map
               google={this.props.google}
               zoom={14}
-              initialCenter={{lat: 40.703316, lng: -73.988145}}
-              center={{lat: 40.703316, lng: -73.988145}}
-              style={{cursor: 'pointer', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', height: '100%'}}
+              initialCenter={wburg}
+              center={wburg}
+              style={{cursor: 'pointer', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}
               >
               {theTrees}
             </Map> }
