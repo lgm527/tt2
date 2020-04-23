c5a65471a2fb726ae860da03d04f8fd9f70ecf58
lgm527 <laurellmccaffrey@gmail.com>
2020-04-23 16:59:38 -0400

    Updates

---
+++ precache-manifest.d36f6441e28d5ba099dd59d434675ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56dcbc266b980a166a52d8a0bbdb8686",
+    "url": "/tt2/index.html"
+  },
+  {
+    "revision": "64af657fd08508e9e897",
+    "url": "/tt2/static/css/main.15c56bed.chunk.css"
+  },
+  {
+    "revision": "056871d7003176440cf7",
+    "url": "/tt2/static/js/2.1bdcf9e7.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/tt2/static/js/2.1bdcf9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64af657fd08508e9e897",
+    "url": "/tt2/static/js/main.ba7d89ff.chunk.js"
+  },
+  {
+    "revision": "430418a3143e69f35033",
+    "url": "/tt2/static/js/runtime-main.0bbe61d0.js"
+  }
+]);
