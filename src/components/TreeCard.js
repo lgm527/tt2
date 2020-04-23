023cb915bd5de4e81b77ff3a78c53dae7c63ae27
lgm527 <laurellmccaffrey@gmail.com>
2020-04-23 15:09:48 -0400

    more styling

--- src/components/TreeCard.js
+++ src/components/TreeCard.js
@@ -13,7 +13,7 @@ export default function TreeCard(props) {
     spc_latin: normalizeString(tree.spc_latin)
   }
 
-  const { health, steward, spc_common, address, zip_city, zipcode, status, spc_latin } = normalizedTree;
+  const { health, steward, spc_common, address, zip_city, zipcode, status, spc_latin, tree_dbh, stump_diam } = normalizedTree;
   const fullAddress = `${address} ${zip_city}, NY ${zipcode}`
 
   return(
@@ -23,10 +23,11 @@ export default function TreeCard(props) {
       </p>
       <p><b>Status:</b> {status}</p>
       <p><b>Health:</b> {health}</p>
-      <p><b>Steward:</b> {steward}</p>
-      <p>Want to take care of this <span role='img' aria-label='tree'>🌳</span>? Share it's address:</p>
+      <p><b>Diameter:</b> {stump_diam === '0' ? tree_dbh : stump_diam} inches</p>
+      <p><b>Signs of Stewardship:</b> {steward}</p>
+      <p>Want to steward this <span role='img' aria-label='tree'>🌳</span>? Share it's address:</p>
       <EmailTree species={spc_common} address={fullAddress}/>
-      <p onClick={backToMap} id='arrow'>⬅︎ back to map ⬅︎</p>
+      <span onClick={backToMap} id='arrow' role='button'>⬅︎ back to map ⬅︎</span>
     </div>
   )
 
